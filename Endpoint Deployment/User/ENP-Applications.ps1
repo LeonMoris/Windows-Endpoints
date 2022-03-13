e2d12d534f29858b04d50946cced575ac632fcf9
Leon Moris <67964202+LeonMoris@users.noreply.github.com>
2022-03-13 21:03:51 +0100

    Update and rename ENP-Applications.ps1 to ENP-UserConfig.ps1

--- Endpoint Deployment/User/ENP-Applications.ps1
+++
@@ -1,9 +0,0 @@
-# Quick Assist
-Write-Host "Creating Quick Assist icon on desktop..."
-$TargetFile = "$env:SystemRoot\System32\quickassist.exe"
-$ShortcutFile = "$env:Public\Desktop\quickassist.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.TargetPath = $TargetFile
-$Shortcut.Save()
-func_sleep
